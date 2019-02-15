38ad699739210e894e0af4d7a0fca2e98464e52a
shiyao666 <1102699514@qq.com>
2019-02-15 19:26:24 +0800

    team_carousel

--- js/soft.js
+++ js/soft.js
@@ -23,3 +23,45 @@ $(document).ready(function () {
         })
     })
 })
+$(document).ready(function () {
+
+    $("#soft_test_act div").click(function () {
+        $(this).addClass("active animated pulse").siblings().removeClass("active animated pulse");
+    });
+})
+// 六个栏目
+$(document).ready(function () {
+
+    $("#soft_team_area>div").click(function () {
+        $(this).addClass("active animated pulse").siblings().removeClass("active animated pulse");
+    });
+})
+// 轮播图
+// 获取页面对应元素
+
+$(document).ready(function () {
+    var left = $("#left");
+    var right = $("#right");
+    var ul = $("#soft_ul");
+    var screen = $("#screen");
+    console.log($("#soft_ul li").length);
+    var index2 = 0;
+    console.log(left);
+    left.click(function () {
+        var clone1 = $("#soft_ul li:eq(0)").clone(true);
+        $('#soft_ul').append(clone1);
+        $('#soft_ul li:eq(0)').remove();
+        console.log($('#soft_ul li:eq(' + index2 + ')'));
+        index2++;
+        // $("#soft_ul").css("left", -(index2 * screen.width() / 4) + 'px');
+        console.log(ul);
+
+        // if (index2 == 3) {
+        //     index2 = 0;
+        //     $("#soft_ul").css("left", 0 + 'px');
+        // }
+    })
+    right.click(function () {
+
+    })
+})
