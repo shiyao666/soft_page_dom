6a93ced1cd0616240aecc622765da8259ef51cfd
shiyao666 <1102699514@qq.com>
2019-02-28 17:27:26 +0800

    2_28

--- js/soft.js
+++ js/soft.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
     // 顶部nav 
     $(window).scroll(function () {
         if ($(window).scrollTop() == 0) {
-            $(".soft_top_tab").css("top", "65px");
-            $(".soft_banner").css("top", "0px");
+            // $(".soft_top_tab").css("top", "65px");
+            // $(".soft_banner").css("top", "0px");
 
         } else {
             $(".soft_top_tab").css("top", "0px");
@@ -18,10 +18,9 @@ $(document).ready(function () {
 })
 $(document).ready(function () {
     $("#tabs ul li").hover(function () {
-        console.log('nice');
         $(this).addClass("active").siblings().removeClass("active");
         var index = $(this).index();
-        $('#tabs_content .soft_lesson_content').eq(index).fadeIn(200).siblings('#tabs_content .soft_lesson_content').hide();
+        $('#tabs_content .soft_lesson_content').eq(index).fadeIn(10).siblings('#tabs_content .soft_lesson_content').hide();
     });
     $("#open_four .soft_open_qi").hover(function () {
         $(this).addClass("active animated pulse").siblings().removeClass("active animated pulse");
@@ -39,7 +38,7 @@ $(document).ready(function () {
 
     $("#open_four button").click(function () {
         var index = $(this).index();
-        console.log(index);
+
         $.ajax({
             type: "post",
             dataType: "json",
@@ -60,7 +59,67 @@ $(document).ready(function () {
 
         })
     })
-    console.log($(".soft_open_qi ").length);
+    // 点击黑色幕布隐藏弹出框
+    $(function () {
+        $("#open_four button").click(function (e) {
+            e.stopPropagation(); //阻止事件向上冒泡
+            $(".soft_alert_box").stop().addClass("active");
+            $(".soft_alert_content").stop().addClass("active");
+            $(document).one("click", function () { //对document绑定一个影藏Div方法
+
+                $(".soft_alert_box").stop().removeClass("active");
+                $(".soft_alert_content").stop().removeClass("active");
+            });
+            $(".soft_alert_content ").click(function (e) {
+                e.stopPropagation();
+            });
+        });
+    });
+    // 
+
+    $("#soft_send_code").click(function () {
+        $.ajax({
+            type: 'post',
+            dataType: 'json',
+            async: 'ture',
+            url: 'http://192.168.4.69:666/activity/enroll/commit',
+            data: {
+                number: $("#soft_phonenum").val(),
+                act_token: 'wD8VlzHoUt'
+            },
+            success: function () {
+
+
+            },
+            error: function () {
+                return false;
+            }
+        })
+    })
+    $("#soft_sumbit_btn").click(function () {
+        $.ajax({
+            type: 'post',
+            dataType: 'json',
+            async: 'ture',
+            url: 'http://192.168.4.69:666/activity/enroll/send_data',
+            data: {
+                number: $("#soft_phonenum").val(),
+                code: $("#soft_code").val(),
+                name: $("#soft_name").val(),
+                company_name: $("#soft_company").val(),
+                act_token: 'wD8VlzHoUt'
+            },
+            success: function () {
+
+
+            },
+            error: function () {
+                return false;
+            }
+        })
+    })
+
+
 
     $.ajax({
 
@@ -69,16 +128,21 @@ $(document).ready(function () {
         async: true,
         url: 'http://192.168.4.69:666/activity/enroll/page',
         data: {
-            "act_token": "number_one"
+            "act_token": "afuxnsd524d"
         },
         success: function (event1) {
-            console.log(event1.data.length);
+            console.log(event1.data.length)
             for (var i = 0; i < event1.data.length; i++) {
-                console.log($(".soft_open_qi").eq(i));
-                console.log($(".soft_open_qi").eq(i).find("ul"));
-                console.log($(".soft_open_qi").eq(i).find("li.soft_name span:eq(0)").text());
-                $(".soft_open_qi").eq(i).find("li.soft_name span:eq(0)").text(event1.data[i].name);
-                console.log(event1.data[i].name);
+                console.log(event1.data[i].activity_content);
+                console.log($(".soft_open_qi").eq(i).find("li.soft_name span:eq(2)"));
+                $(".soft_open_qi").eq(i).find("li.soft_content1 span:eq(0)").text(event1.data[i].activity_content.title1);
+                $(".soft_open_qi").eq(i).find("li.soft_content1 span:eq(1)").text(event1.data[i].activity_content.content1);
+                $(".soft_open_qi").eq(i).find("li.soft_content2 span:eq(0)").text(event1.data[i].activity_content.title2);
+                $(".soft_open_qi").eq(i).find("li.soft_content2 span:eq(1)").text(event1.data[i].activity_content.content2);
+                $(".soft_open_qi").eq(i).find("li.soft_content3 span:eq(0)").text(event1.data[i].activity_content.title3);
+                $(".soft_open_qi").eq(i).find("li.soft_content3 span:eq(1)").text(event1.data[i].activity_content.content3);
+                $(".soft_open_qi").eq(i).find("li.soft_content4 span:eq(0)").text(event1.data[i].activity_content.title4);
+                $(".soft_open_qi").eq(i).find("li.soft_content4 span:eq(1)").text(event1.data[i].activity_content.content4);
             }
 
         },
@@ -104,16 +168,15 @@ $(document).ready(function () {
     $("#soft_team_area>div").hover(function () {
         $(this).addClass("active animated pulse").siblings().removeClass("active animated pulse");
     });
-    $("#soft_act_area span").click(function () {
+    $("#soft_act_area span").hover(function () {
         $(this).addClass("active").siblings().removeClass("active");
         var index = $(this).index();
-        console.log(index);
-        console.log($(".soft_act_tab").eq(index));
 
         $(".soft_act_tab").eq(index).addClass("move-in").siblings().removeClass("move-in");
         setTimeout(function () {
             $(".soft_act_tab").eq(index).addClass("active").siblings().removeClass("active"), 300
         })
+        clearTimeout();
     })
 })
 // 鼠标滚轮
@@ -131,16 +194,12 @@ $(document).ready(function () {
             $(".soft_top_tab ").addClass("hide1").removeClass("active default");
         }
         if (event.deltaY == 1) {
-            $(".soft_top_tab ").addClass("active").removeClass("hide default");
+            $(".soft_top_tab ").addClass("active").removeClass("hide1 default");
         }
     });
     var ol = $("#soft_top_tab")[0];
-    console.log(ol);
     var olLiArr = ol.children;
-    console.log(olLiArr);
     var floor = $(".soft_floor");
-    console.log(olLiArr[0].index);
-    console.log(floor[2]);
 
     var timer = null,
         leader = 0,
