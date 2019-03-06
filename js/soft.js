b740d7da223ac6fade82e7e5e3cd61448ebaf9eb
shiyao666 <1102699514@qq.com>
2019-03-06 17:22:28 +0800

    add_css_style

--- js/soft.js
+++ js/soft.js
@@ -154,6 +154,7 @@ $.ajax({
             $(".soft_open_qi").eq(i).find("li.soft_content4 span:eq(0)").text(event1.data[i].activity_content.title4);
             $(".soft_open_qi").eq(i).find("li.soft_content4 span:eq(1)").text(event1.data[i].activity_content.content4);
             $(".soft_open_qi").eq(i).find(".soft_set_info b").text(event1.data[i].max_number - event1.data[i].sign_up_number);
+            $(".soft_open_qi").eq(i).find(".soft_qi_num").text(event1.data[i].activity_content.head_name);
             setCookie("page_token" + [i] + "", Array(event1.data[i].page_token));
         }
 
@@ -177,7 +178,7 @@ $(document).ready(function () {
         $(this).addClass("active").siblings().removeClass("active");
     })
     $("#soft_team_area>div").hover(function () {
-        $(this).addClass("active animated pulse").siblings().removeClass("active animated pulse");
+        $(this).addClass("active").siblings().removeClass("active");
     });
     $("#soft_act_area span").hover(function () {
         $(this).addClass("active").siblings().removeClass("active");
@@ -190,9 +191,6 @@ $(document).ready(function () {
         clearTimeout();
     })
     // 模拟测试
-    $("#soft_test_btn").click(function () {
-        window.location.href = "www.baidu.com";
-    })
 })
 // 鼠标滚轮
 $(document).ready(function () {
@@ -252,64 +250,69 @@ $(document).ready(function () {
     }
 })
 /** 
-* @author xuesong
-* @time  2019-03-05 
-*/
+ * @author xuesong
+ * @time  2019-03-05 
+ */
 function getDocumentTop() {
-    var scrollTop = 0, bodyScrollTop = 0, documentScrollTop = 0;
+    var scrollTop = 0,
+        bodyScrollTop = 0,
+        documentScrollTop = 0;
     if (document.body) {
-       bodyScrollTop = document.body.scrollTop;
+        bodyScrollTop = document.body.scrollTop;
     }
     if (document.documentElement) {
-       documentScrollTop = document.documentElement.scrollTop;
+        documentScrollTop = document.documentElement.scrollTop;
     }
-    scrollTop = (bodyScrollTop - documentScrollTop > 0) ? bodyScrollTop : documentScrollTop;    return scrollTop;
-    }
+    scrollTop = (bodyScrollTop - documentScrollTop > 0) ? bodyScrollTop : documentScrollTop;
+    return scrollTop;
+}
 /** 
-* @author xuesong
-* @time  2019-03-05 
-*/
-    //可视窗口高度
-    function getWindowHeight() {
-    
-    var windowHeight = 0;    if (document.compatMode == "CSS1Compat") {
-       windowHeight = document.documentElement.clientHeight;
+ * @author xuesong
+ * @time  2019-03-05 
+ */
+//可视窗口高度
+function getWindowHeight() {
+
+    var windowHeight = 0;
+    if (document.compatMode == "CSS1Compat") {
+        windowHeight = document.documentElement.clientHeight;
     } else {
-       windowHeight = document.body.clientHeight;
+        windowHeight = document.body.clientHeight;
     }
     return windowHeight;
-    
-    }
+
+}
 /** 
-* @author xuesong
-* @time  2019-03-05 
-*/ 
-    //滚动条滚动高度
-    function getScrollHeight() {
-    var scrollHeight = 0, bodyScrollHeight = 0, documentScrollHeight = 0;
+ * @author xuesong
+ * @time  2019-03-05 
+ */
+//滚动条滚动高度
+function getScrollHeight() {
+    var scrollHeight = 0,
+        bodyScrollHeight = 0,
+        documentScrollHeight = 0;
     if (document.body) {
-       bodyScrollHeight = document.body.scrollHeight;
+        bodyScrollHeight = document.body.scrollHeight;
     }
     if (document.documentElement) {
-       documentScrollHeight = document.documentElement.scrollHeight;
+        documentScrollHeight = document.documentElement.scrollHeight;
     }
-    scrollHeight = (bodyScrollHeight - documentScrollHeight > 0) ? bodyScrollHeight : documentScrollHeight;    
+    scrollHeight = (bodyScrollHeight - documentScrollHeight > 0) ? bodyScrollHeight : documentScrollHeight;
     return scrollHeight;
-    }
-    window.onscroll = function () {
+}
+window.onscroll = function () {
     //监听事件内容
-    if(getScrollHeight() == getWindowHeight() + getDocumentTop()){
-        //当滚动条到底时,这里是触发内容
-        if(!localStorage.user_token){
-            var password = Date.parse(new Date()).toString()+Math.ceil(Math.random()*10000).toString();
+    if (getScrollHeight() == getWindowHeight() + getDocumentTop()) {
+        //当滚动条到底时,这里是触发内容
+        if (!localStorage.user_token) {
+            var password = Date.parse(new Date()).toString() + Math.ceil(Math.random() * 10000).toString();
             //加密成md5
             var passwd = $.md5(password);
-            localStorage.setItem("user_token",passwd)
+            localStorage.setItem("user_token", passwd)
         }
         //get请求,用于统计页面访问数
-        $.get("http://192.168.60.175:666/activity/enroll/browse&user_token="+localStorage.user_token+"&act_token=afuxnsd524d",function(data,status){
+        $.get("http://192.168.60.175:666/activity/enroll/browse&user_token=" + localStorage.user_token + "&act_token=afuxnsd524d", function (data, status) {
             // alert("数据：" + data + "\n状态：" + status);
         });
-        }
     }
-    
+}
